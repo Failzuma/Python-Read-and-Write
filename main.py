53e7712a21bb939b93856c1fca55b81c10d381ec
Failzuma <ibnuilham720@gmail.com>
2023-03-04 00:11:16 +0700

    Init

---
+++ main.py
@@ -0,0 +1,8 @@
+import pymem, utility
+
+process = pymem.Pymem("ac_client.exe")
+current_ammo_base = process.base_address + 0x0016F338
+ammo_decrement = process.base_address + 0xC73EF
+current_ammo = utility.FindDMAAddy(process.process_handle, current_ammo_base, [0x0, 0xB8, 0x140], 32)
+process.write_bytes(ammo_decrement, b"9090", 2)
+process.write_int(current_ammo, 9999)
